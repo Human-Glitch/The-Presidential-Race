c80443f726906bdcadb9fee7f995a1153b05c1fe
James Essmiller <jamestoast@cox.net>
2016-04-13 00:17:03 -0500

    Added some basic kart control stuff, and added a test track based on the terain stuff from class. It's actually a lot of fun.

---
+++ Assets/Scripts/KartMovement.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KartMovement : MonoBehaviour {
+
+
+	[SerializeField] private float accRate = 1f;
+	[SerializeField] private float maxSpeed = 15f;
+	[SerializeField] private float turnRate = 3f;
+
+	public float inputMask = 1f;
+	//[SerializeField] private UIScript userInterf;
+
+	private Rigidbody rb;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void Accelerate(float accInput) {
+		accInput *= inputMask;
+		Vector3 velocity = rb.velocity;
+		velocity += (transform.forward * accInput * accRate);
+		if (velocity.magnitude < maxSpeed)
+			rb.velocity = velocity;
+		else if (rb.velocity.magnitude >= 0f)
+			rb.velocity = rb.velocity.normalized * maxSpeed;
+		else
+			rb.velocity = Vector3.zero;
+
+		float y = rb.velocity.y;
+		velocity = rb.velocity;
+		velocity.y = 0f;
+		velocity = transform.forward * velocity.magnitude;
+		velocity.y = y;
+		rb.velocity = velocity;
+		print (velocity);
+		//this.velocity = velocity * inputMask;
+	}
+
+	public void Turn(float xInput) {
+		xInput *= inputMask;
+		print (xInput);
+		transform.Rotate (0, xInput * turnRate * rb.velocity.magnitude, 0);
+		/*Vector3 rotation = transform.rotation.eulerAngles;
+		rotation.y += xInput * turnRate * rb.velocity.magnitude;
+		transform.rotation.eulerAngles = rotation;*/
+		//transform.rotation.eulerAngles.y += xInput * turnRate * rb.velocity.magnitude;
+	}
+
+	/*public void Rotate(Vector3 rotation)
+	{
+		this.rotation = rotation * inputMask;
+	}*/
+
+	void FixedUpdate() {
+		//rb.MovePosition (rb.position + velocity * Time.fixedDeltaTime);
+		//Vector3 localRotation = new Vector3 (rotation.x, 0, 0);
+		//Vector3 globalRotation = new Vector3 (0, rotation.y, 0);
+		/*rb.MoveRotation (rb.rotation * Quaternion.Euler (rotation));
+
+		if (inputMask > 0 && rb.position.y < 0) {
+			inputMask = 0f;
+			userInterf.Win ();
+		}*/
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent(typeof(KartMovement))]
+
+public class PlayerController : MonoBehaviour {
+
+	private KartMovement movement;
+
+
+	// Use this for initialization
+	void Start () {
+		movement = GetComponent<KartMovement> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float acc = Input.GetAxis ("Accelerate");
+		float x = Input.GetAxis ("Horizontal");
+		//if (acc == 0)
+		//	acc = x;
+		//Vector3 acc = transform.right * z;
+		//Vector3 movVert = transform.forward * z;
+		//Vector3 velocity = (movHoriz + movVert).normalized;
+
+		movement.Accelerate (acc);
+		movement.Turn (x);
+		/*
+		float yRot = Input.GetAxis ("Mouse X");
+		float xRot = 0;//-Input.GetAxis ("Mouse Y");
+		Vector3 rotation = new Vector3 (xRot, yRot, 0);
+		movement.Rotate (rotation);*/
+	}
+}
