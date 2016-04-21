8ecea1ea41c865758eded23d9c49634d6349e0e9
Dennis Bond <dennis.bond@okstate.edu>
2016-04-20 20:10:26 -0500

    Here is some work on the title screen

---
+++ Assets/Scripts/CreditUI.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class CreditUI : MonoBehaviour {
+	public Button back;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void Back(){
+		//Load Title Screen
+		SceneManager.LoadScene("Title");
+		print ("Back");
+	
+	}
+}
---
+++ Assets/Scripts/TitleUI.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class TitleUI : MonoBehaviour {
+	public Button letsRace, credits, options, quit;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Title Screen Buttons
+	public void LetsRace(){
+		print ("Lets Race!");
+		//Load character select scene
+	}
+
+	public void Credits(){
+		print ("Credits");
+		//load credit scene
+		SceneManager.LoadScene("Credits");	
+	}
+
+	public void Options(){
+		print ("Options");
+		// display options
+	}
+
+	public void Quit(){
+		print ("Quit");
+		// quit the application
+	}
+		
+}
